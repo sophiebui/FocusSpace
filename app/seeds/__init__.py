b93afaa4a61fcc6ee86b756282767710f3a38e91
Sophia <sophiatbui@gmail.com>
2022-02-15 23:16:03 -0700

    completed seed data for places and began seed data for reviews

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,8 +1,10 @@
+import imp
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .places import seed_places, undo_places
 from .bookings import seed_bookings, undo_bookings
-
+from .images import seed_images, undo_images
+from .reviews import seed_reviews, undo_reviews
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
 seed_commands = AppGroup('seed')
@@ -14,12 +16,16 @@ def seed():
     seed_users()
     seed_places()
     seed_bookings()
+    seed_images()
+    seed_reviews()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_reviews()
+    undo_images()
     undo_bookings()
     undo_places()
     undo_users()
