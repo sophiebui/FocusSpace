422c9a28b1b98588f275e1e40c5a362742205fc5
Sophia <sophiatbui@gmail.com>
2022-02-16 15:20:14 -0700

    completed styling for signup/login modals

--- react-app/src/components/LoginFormModal/LoginForm.js
+++ react-app/src/components/LoginFormModal/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { Redirect, NavLink } from 'react-router-dom';
 import { login } from '../../store/session';
 
 const LoginForm = ({ setShowModal }) => {
@@ -30,37 +30,38 @@ const LoginForm = ({ setShowModal }) => {
 	};
 
 	if (user) {
-		return <Redirect to="/" />;
+		return <Redirect to='/' />;
 	}
 
 	return (
-		<div className="form-container">
-			<form className="form" onSubmit={onLogin}>
-				{/* <div className="error-list">{errors[0]}</div> */}
-				<h1>Log In</h1>
-				<label htmlFor="email"> </label>
-				<input
-					placeholder="Email"
-					name="email"
-					type="text"
-					value={email}
-					onChange={updateEmail}
-					className="input"
-				/>
-				<label htmlFor="password" />
-				<input
-					placeholder="Password"
-					name="password"
-					type="password"
-					value={password}
-					onChange={updatePassword}
-					className="input"
-				/>
-				<button className="form-button" type="submit">
-					Login
-				</button>
-			</form>
-		</div>
+        <div className='form-container'>
+            <form className='form' onSubmit={onLogin}>
+                {/* <div className='error-list'>{errors[0]}</div> */}
+                <h1 className='form-title'>Welcome to FocusSpace</h1>
+                <label htmlFor='email'> </label>
+                <input
+                    placeholder='Email'
+                    name='email'
+                    type='text'
+                    value={email}
+                    onChange={updateEmail}
+                    className='input'
+                />
+                <label htmlFor='password' />
+                <input
+                    placeholder='Password'
+                    name='password'
+                    type='password'
+                    value={password}
+                    onChange={updatePassword}
+                    className='input'
+                />
+                <button className='form-button' type='submit'>
+                    Login
+                </button>
+
+            </form>
+        </div>
 	);
 };
 
