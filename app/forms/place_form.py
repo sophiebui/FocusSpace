c7e1603e462b4e584efeb5b6a52078d8d7633e1b
Sophia <sophiatbui@gmail.com>
2022-02-17 10:05:44 -0700

    completed forms for place

---
+++ app/forms/place_form.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, IntegerField
+from wtforms.validators import DataRequired, Length, EqualTo
+
+class PlaceForm(FlaskForm):
+    user_id = IntegerField("", validators={DataRequired()})
+    name = StringField("Name: ", validators=[DataRequired(), Length(min=2, max=64, message="Name must be within 2 to 64 characters")])
+    description = StringField("Description: ", validators=[Length(max=256, message="Description must be less than 256 characters")])
+    address = StringField("Address: ", validators={DataRequired()})
+    city = StringField("City: ", validators={DataRequired()})
+    state = StringField("State: ", validators={DataRequired()})
+    zip_code = StringField("Zip Code: ",  validators=[DataRequired(), Length(min=5, message="Zip code must be 5 characters")])
+    price = IntegerField("Price: ", validators={DataRequired()})
+    guests = IntegerField("Address: ", validators={DataRequired()})
+    submit = SubmitField("Submit")
+
+
+class DeletePlaceForm(FlaskForm):
+    place_id = IntegerField("", validators=[DataRequired()])
+    curr_user_id = IntegerField("", validators=[DataRequired(), EqualTo('curr_user_id', message='You are not authorized to delete this listing')])
+    place_user_id = IntegerField("", validators=[DataRequired(), EqualTo('place_user_id', message='You are not authorized to delete this listing')])
+    submit = SubmitField("Submit")
