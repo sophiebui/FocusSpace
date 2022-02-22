c038b54646d8c6e4470375a1ec9519f6372ba55f
Sophia <sophiatbui@gmail.com>
2022-02-21 17:43:30 -0700

    added conditional for light/dark navbar

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { Modal } from '../../context/Modal';
 import { login } from '../../store/session';
-// import logoLight from '../../assets/logo-light.PNG';
+import logoLight from '../../assets/logo-light.PNG';
 import logoDark from '../../assets/logo-no-bkgd.PNG';
 import bars from '../../assets/bars-solid.svg';
 import userLogo from '../../assets/user.svg';
@@ -17,6 +17,8 @@ const NavBar = () => {
     const [ profileButton, setProfileButton ] = useState(false);
 	const [ loginModal, setLoginModal ] = useState(false);
 	const [ signupModal, setSignupModal ] = useState(false);
+    const location = useLocation()
+    console.log(location.pathname)
 
 	const demoLogin = () => {
 		const email = 'demo@aa.io';
@@ -28,10 +30,10 @@ const NavBar = () => {
     return (
         <>
             <nav>
-                <ul className='navbar'>
+                <ul className={`${location.pathname === '/' ? 'dark-navbar' : 'light-navbar' }`}>
                     <li>
                         <NavLink to='/'>
-                            <img src={logoDark} alt='FocusPlace logo' className='logo' />
+                            <img src={`${location.pathname ==='/' ? logoDark : logoLight}`} alt='FocusPlace logo' className='logo' />
                         </NavLink>
                     </li>
                     <li>
