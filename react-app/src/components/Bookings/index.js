1ffbe94fe4ccacc58515124b41b3ef0474a80836
Sophia <sophiatbui@gmail.com>
2022-02-23 17:55:29 -0700

    added error message if you do not have any bookings

--- react-app/src/components/Bookings/index.js
+++ react-app/src/components/Bookings/index.js
@@ -18,50 +18,55 @@ const Bookings = () => {
     },[ dispatch, userId])
 
     if (isOwner) {
-        return (
-            <div className='page-container'>
-                {bookings?.map((booking) => (
-                    <div key={booking.id} className='bookings-container'>
-                        <div key={booking.place.images.id}  className='booking-image-container'>
-                            <img src={booking.place.images[0].url}
-                            className='booking-image'
-                            onError={event => {
-                                event.target.src = "https://res.cloudinary.com/dxubahnmi/image/upload/v1644967329/FocusSpace/1-default.jpg"
-                                event.onerror = null
-                            }} />
+        if (bookings.length > 0) {
+            return (
+                <div className='page-container'>
+                    {bookings?.map((booking) => (
+                        <div key={booking.id} className='bookings-container'>
+                            <div key={booking.place.images.id}  className='booking-image-container'>
+                                <img src={booking.place.images[0].url}
+                                className='booking-image'
+                                onError={event => {
+                                    event.target.src = "https://res.cloudinary.com/dxubahnmi/image/upload/v1644967329/FocusSpace/1-default.jpg"
+                                    event.onerror = null
+                                }} />
+                            </div>
+                            <div className='bookings-detail'>
+                                <div key={booking.place.name}>
+                                    Name of Place: {booking.place.name}
+                                </div>
+                                <div key={booking.place.address}>
+                                    Address: {booking.place.address}
+                                </div>
+                                <div key={booking.date}>
+                                    Date: {booking.date}
+                                </div>
+                                <div key={booking.time}>
+                                Time:  {booking.time}
+                                </div>
+                                <div key={booking.duration}>
+                                Duration: {booking.duration}
+                                </div>
+                                <div key={booking.guests}>
+                                # of Guests: {booking.guests}
+                                </div>
+                            </div>
+                                <div>
+                                    <EditBookingForm booking={booking}/>
+                                </div>
                         </div>
-                        <div className='bookings-detail'>
-                            <div key={booking.place.name}>
-                                Name of Place: {booking.place.name}
-                            </div>
-                            <div key={booking.place.address}>
-                                Address: {booking.place.address}
-                            </div>
-                            <div key={booking.date}>
-                                Date: {booking.date}
-                            </div>
-                            <div key={booking.time}>
-                            Time:  {booking.time}
-                            </div>
-                            <div key={booking.duration}>
-                            Duration: {booking.duration}
-                            </div>
-                            <div key={booking.guests}>
-                            # of Guests: {booking.guests}
-                            </div>
-                        </div>
-                            <div>
-                                <EditBookingForm booking={booking}/>
-                            </div>
-                    </div>
-                ))}
-            </div>
-        )
+                    ))}
+                </div>
+            )
     } else {
         return (
-            <h1>You are not authorized to view this page</h1>
+            <h1 className='bookings-page-error'> You do not own any bookings </h1>
         )
     }
-};
+ } else {
+    return (
+        <h1 className='bookings-page-error'>You are not authorized to view this page</h1>
+    )
+}; }
 
 export default Bookings;
