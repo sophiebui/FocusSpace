eaa0c2ff2afc5da2bcfc1e56b3ad08e2f276684b
Sophia <sophiatbui@gmail.com>
2022-02-23 18:20:04 -0700

    added react-alert

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
 import './index.css';
 import App from './App';
 import configureStore from './store';
@@ -8,12 +10,24 @@ import { ModalProvider } from './context/Modal';
 
 const store = configureStore();
 
+// optional config for react-alert
+const options = {
+    // you can also just use 'bottom center'
+    position: 'top right',
+    timeout: 4000,
+    offset: '50px',
+    // you can also just use 'scale'
+    transition: transitions.SCALE
+  }
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-        <ModalProvider>
-            <App />
-        </ModalProvider>
+        <AlertProvider template={AlertTemplate} {...options}>
+            <ModalProvider>
+                <App />
+            </ModalProvider>
+        </AlertProvider>
       </Provider>
   </React.StrictMode>,
   document.getElementById('root')
