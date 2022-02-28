6d6c9dd0ea324480a541f383aa32969d922c9bfe
Sophia <sophiatbui@gmail.com>
2022-02-27 21:44:02 -0700

    completed styling for delete modal

--- react-app/src/components/DeletePlaceModal/DeletePlaceForm.js
+++ react-app/src/components/DeletePlaceModal/DeletePlaceForm.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { removePlace } from "../../store/places";
+import { removePlace } from '../../store/places';
 import { useHistory } from 'react-router-dom';
 
 function DeletePlaceForm({place, setShowModal}) {
@@ -33,10 +33,12 @@ function DeletePlaceForm({place, setShowModal}) {
 
     return (
         <div className='delete'>
-        <h2>Are you sure you want to delete this place?</h2>
-        <h3>This cannot be undone.</h3>
-        <button type="button" onClick={(e) => submitDelete()} className='delete-submit-button'>Yes</button>
-        <button type="button" onClick={(e) => setShowModal(false)} className='delete-cancel-button'>No</button>
+        <p className='delete-message'>Are you sure you want to remove this listing?</p>
+        <p className='delete-note'>Please note: this action cannot be undone.</p>
+        <div className='delete-button-div'>
+            <button type='button' onClick={(e) => submitDelete()} className='delete-place-submit-button'>Yes</button>
+            <button type='button' onClick={(e) => setShowModal(false)} className='delete-place-cancel-button'>Cancel</button>
+        </div>
     </div>)
 }
 
