2a3cd27060a20c770cfdcdc65a8a69c1dd9f801a
Sophia <sophiatbui@gmail.com>
2022-04-11 15:33:44 -0700

    added backend validation for search form

--- app/forms/search_form.py
+++ app/forms/search_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, DateField, TimeField
-from wtforms.validators import DataRequired, ValidationError
+from wtforms.validators import DataRequired, ValidationError, NumberRange
 import datetime
 
 def validate_date(form, field):
@@ -10,6 +10,6 @@ def validate_date(form, field):
 
 class SearchForm(FlaskForm):
     location = StringField("", validators=[DataRequired()])
-    date = DateField("date", validators=[DataRequired(), validate_date])
-    time = TimeField("time", validators={DataRequired()})
-    guests = IntegerField("", validators=[DataRequired()])
+    date = DateField("date", validators=[DataRequired(message="Date field is required"), validate_date])
+    time = TimeField("time", validators={DataRequired(message="Time field is required")})
+    guests = IntegerField("guests: ", validators=[DataRequired(message="Number of guest cannot be blank"), NumberRange(min=1, message="Number of guests must be greater than 0"), NumberRange(max=2000, message="Number of guests must not exceed 2000")])
